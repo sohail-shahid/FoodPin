80bd0b185c02fd4b39867e5102d85793146f043e
Sohail khan <sohail.khan@discorp.com>
2023-07-11 23:59:31 +0500

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by CRLHL-KHANNSOH2 on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
